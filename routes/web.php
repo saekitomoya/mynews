9d8aaa64cdaa7a8dadeac400dd295aa0601d926c
saekitomoya <wbc_tomoya@yahoo.co.jp>
2022-01-21 05:41:57 +0000

    message

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,13 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add');
+});
+
+Route::get('XXX','Admin\AAAController@bbb');
+ 
+ 
+Route::get('admin/profile/create','Admin\ProfileController@add');
+
+Route::get('admin/profile/edit','Admin\ProfileController@edit');
