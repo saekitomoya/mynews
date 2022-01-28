061fc9f009c1b4cd805f8dbf11c66facf1065345
saekitomoya <wbc_tomoya@yahoo.co.jp>
2022-01-28 05:13:32 +0000

    message

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
 });
 
 Route::get('XXX','Admin\AAAController@bbb');
  
  
-Route::get('admin/profile/create','Admin\ProfileController@add');
+Route::get('admin/profile/create','Admin\ProfileController@add')->middleware('auth');
 
-Route::get('admin/profile/edit','Admin\ProfileController@edit');
+Route::get('admin/profile/edit','Admin\ProfileController@edit')->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
