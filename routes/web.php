c1263f4301ef927ecd5b17bab616bc1914a2aef4
saekitomoya <wbc_tomoya@yahoo.co.jp>
2022-02-28 11:59:02 +0000

    message

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,8 @@ Route::get('/home', 'HomeController@index')->name('home');
     Route::get('profile/edit', 'Admin\ProfileController@edit')->middleware('auth'); // 追記
     Route::post('profile/edit', 'Admin\ProfileController@update')->middleware('auth'); // 追記
     Route::get('profile/delete', 'Admin\ProfileController@delete')->middleware('auth');
-
+    
 });
     
+    Route::get('/', 'NewsController@index');
+    Route::get('/profile', 'ProfileController@index');
